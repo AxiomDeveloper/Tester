3328fb24011d7407a606a3e58a9d5a57b9dc0d07
AxiomDeveloper <sqtsqqhtkx@privaterelay.appleid.com>
2026-02-14 11:51:24 +0000

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: Replace 'REPO_NAME' with your GitHub repository name
-  base: '/Tester/',
-})
